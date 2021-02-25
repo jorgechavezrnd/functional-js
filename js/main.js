d54f254cf70274545fb5070ae03ff8e3a0c69858
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-02-25 18:06:32 -0400

    ids and variables for inputs, added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+let description = $('#description');
+let carbs = $('#carbs');
+let calories = $('#calories');
+let protein = $('#protein');
